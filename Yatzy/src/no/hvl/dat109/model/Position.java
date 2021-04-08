e424d7867205c0eb17aefcffbc13d149f6c223d2
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-08 12:06:09 +0200

    Lagt til metoder i Position klassen

--- Yatzy/src/no/hvl/dat109/model/Position.java
+++ Yatzy/src/no/hvl/dat109/model/Position.java
@@ -1,8 +1,29 @@
 package no.hvl.dat109.model;
 
+// Klassen brukes i Servleten for å holde styr på framgangen i spillet
 public class Position {
 	
 	private int row;
 	private int player;
 	
+	public Position(int row, int player) {
+		this.row = row;
+		this.player = player;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
 }
