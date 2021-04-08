b34836568f52ea13c3ef8c198e9d8b67550a181b
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-08 19:59:33 +0200

    Lagt til RegisterServlet

---
+++ Yatzy/src/no/hvl/dat109/servlets/RegisterServlet.java
@@ -0,0 +1,24 @@
+package no.hvl.dat109.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet(name = "RegisterServlet", urlPatterns = { "/register" })
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+}
