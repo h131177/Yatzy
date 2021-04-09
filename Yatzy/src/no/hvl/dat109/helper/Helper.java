847ce0679885a713ffba279b4021bcc8ce5a724d
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-10 00:24:21 +0200

    Lagt til metode som regner ut poengsum

--- Yatzy/src/no/hvl/dat109/helper/Helper.java
+++ Yatzy/src/no/hvl/dat109/helper/Helper.java
@@ -1,6 +1,17 @@
 package no.hvl.dat109.helper;
 
+import java.util.List;
+
 // Klasse med statiske hjelpemetoder for å regne ut poengsummen
 public class Helper {
-
+	
+	public static int calculatePoints(int n, List<Integer> list) {
+		int result = 0;
+		for (int i : list) {
+			if(i == n) {
+				result += i;
+			}
+		}
+		return n;
+	}
 }
