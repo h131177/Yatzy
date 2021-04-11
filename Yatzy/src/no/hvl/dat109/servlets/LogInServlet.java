bc8606836aa87842d94d5dc4927b2b34215b9ac3
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-12 00:18:05 +0200

    Login redirecter til game siden

--- Yatzy/src/no/hvl/dat109/servlets/LogInServlet.java
+++ Yatzy/src/no/hvl/dat109/servlets/LogInServlet.java
@@ -7,19 +7,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import no.hvl.dat109.model.Game;
+import no.hvl.dat109.model.Player;
+
 /**
  * Servlet implementation class LogInServlet
  */
 @WebServlet(name = "LogInServlet", urlPatterns = { "/login" })
 public class LogInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	Player user;
+	Game game;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("WEB-INF/jsp/login.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.getSession().setAttribute("loggedIn", user);
+		game.addPlayer(user);
+		response.sendRedirect("game");
 	}
 
 }
