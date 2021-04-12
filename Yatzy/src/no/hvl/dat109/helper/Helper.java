292c983ab91d7c218d9e470e8948688cef136e4d
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-12 22:54:32 +0200

    Endret parameter fra Integer til Dice list

--- Yatzy/src/no/hvl/dat109/helper/Helper.java
+++ Yatzy/src/no/hvl/dat109/helper/Helper.java
@@ -2,14 +2,16 @@ package no.hvl.dat109.helper;
 
 import java.util.List;
 
+import no.hvl.dat109.model.Dice;
+
 // Klasse med statiske hjelpemetoder for å regne ut poengsummen
 public class Helper {
 	
-	public static int calculatePoints(int n, List<Integer> list) {
+	public static int calculatePoints(int n, List<Dice> list) {
 		int result = 0;
-		for (int i : list) {
-			if(i == n) {
-				result += i;
+		for (Dice d : list) {
+			if(d.getValue() == n) {
+				result += d.getValue();
 			}
 		}
 		return n;
