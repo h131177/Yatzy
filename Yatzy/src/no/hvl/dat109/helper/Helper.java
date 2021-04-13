09ff2dabb2f2483760cb170e5e6482001983016e
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-13 15:57:05 +0200

    Lagt til getInfo metode som returnerer liste ones, twos osv

--- Yatzy/src/no/hvl/dat109/helper/Helper.java
+++ Yatzy/src/no/hvl/dat109/helper/Helper.java
@@ -1,5 +1,6 @@
 package no.hvl.dat109.helper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import no.hvl.dat109.model.Dice;
@@ -15,5 +16,17 @@ public class Helper {
 			}
 		}
 		return n;
+	}
+	
+	public static List<String> getInfo() {
+		List<String> infoList = new ArrayList<>();
+		infoList.add("Ones");
+		infoList.add("Twos");
+		infoList.add("Threes");
+		infoList.add("Fours");
+		infoList.add("Fives");
+		infoList.add("Sixes");
+		infoList.add("Sum");
+		return infoList;
 	}
 }
