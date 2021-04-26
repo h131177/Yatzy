9c882320a7536ef5dfbc7efcbfc06172d9e745df
h131177 <54102543+h131177@users.noreply.github.com>
2021-04-26 18:08:25 +0200

    Lagt til TODO

--- Yatzy/src/no/hvl/dat109/servlets/LogOutServlet.java
+++ Yatzy/src/no/hvl/dat109/servlets/LogOutServlet.java
@@ -15,17 +15,12 @@ import javax.servlet.http.HttpServletResponse;
 public class LogOutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//TODO Nullstille 
 		request.getRequestDispatcher("WEB-INF/jsp/logout.jsp").forward(request, response);
 	}
 
